a31d8684865c8d977cf1d2e957cfef17ccc35efe
Miguel Angel Quinones Garcia <m.quinones.garcia@gmail.com>
2016-04-18 15:14:07 +0100

    Temporary http injection

---
+++ Dynamic Code Injection/dyci/Classes/SFHTTPCodeInjection.h
@@ -0,0 +1,13 @@
+//
+//  SFHTTPCodeInjection.h
+//  dyci
+//
+//  Created by Miguel on 26/02/2016.
+//  Copyright © 2016 Stanfy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFHTTPCodeInjection : NSObject
+
+@end
---
+++ Dynamic Code Injection/dyci/Classes/SFHTTPCodeInjection.m
@@ -0,0 +1,13 @@
+//
+//  SFHTTPCodeInjection.m
+//  dyci
+//
+//  Created by Miguel on 26/02/2016.
+//  Copyright © 2016 Stanfy. All rights reserved.
+//
+
+#import "SFHTTPCodeInjection.h"
+
+@implementation SFHTTPCodeInjection
+
+@end
